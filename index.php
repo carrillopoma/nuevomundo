65bf2cb98bfb59c90a22a9098b22eb8d40941352
abimelec <carrillopoma@gmail.com>
2016-03-04 10:58:10 -0500

    primer proyecto

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<h1> beivenido al nuevo mundo</h1>
+	</body>
+</html>	
+</nav>
+<div class="slider">
+<img src="carrillo.jpg" heigth="900" width="900">
+</div>
+</body>
+</html>
